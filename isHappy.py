044e983b27ebec10305678fc469353661e35ef3f
vlukic973 <42998963+vlukic973@users.noreply.github.com>
2023-01-19 11:44:18 +0100

    Add files via upload

---
+++ isHappy.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        seen = set()
+        while n not in seen:
+            seen.add(n)
+            n = sum(int(i)**2 for i in str(n))
+            if n == 1:
+                return True
+        return False
